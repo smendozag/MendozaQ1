8d1fb02e34aefb5385f43cecc46e4715136b0630
SERGIO MENDOZA GOMEZ <smendozagomez@cnm.edu>
2020-06-18 18:50:45 -0600

    Add project files.

---
+++ Force.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MendozaQ1
+{
+
+
+   
+    class Force
+    {
+        private const double gravityConst = 0.0000000000667408;
+
+        private double force;
+        private double objectMass1;
+        private double objectMass2;
+        private double distanceApart;
+        public string objectName1;
+        public string objectName2;
+
+        public double ObjectMass1
+        {
+            get { return objectMass1; }
+            set { objectMass1 = value; calc(); }
+        }
+
+      
+        public string ObjectName1
+        {
+            get { return objectName1; }
+            set { objectName1 = value; }
+        }
+
+        
+
+        public string ObjectName2
+        {
+            get { return objectName2; }
+            set { objectName2 = value; }
+        }
+
+        public double ObjectMass2
+        {
+            get { return objectMass2; }
+            set { objectMass2 = value; calc(); }
+
+        }
+
+        public double Radius
+        {
+            get { return distanceApart; }
+
+            set { distanceApart = value; calc(); }
+        }
+        public double GravityForce { get; private set; }
+
+        private void calc()
+        {
+            force = gravityConst * ((objectMass1 * objectMass2) / distanceApart);
+
+        }
+
+        public override string ToString()
+        {
+            return "The force between " + objectName1 + " and " + objectName2 + " is: " + force + " in Newtons.";
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Sergio Mendoza Gomez  |   smendozagomez@cnm.edu
+ * File: MendozaQ1
+ * Date: 06//2020
+ */
+using System;
+
+namespace MendozaQ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string doAgain = "y";
+            do
+            {
+                //instantiate Force
+                Force newForce = new Force();
+
+                //inputs and assigning them into the object
+                Console.Write("Please type in the name of the first object: ");
+                string name1 = Console.ReadLine();
+
+                Console.Write("Please type in the mass of the first object in (Kg): ");
+                double  mass = double.Parse(Console.ReadLine());
+                newForce.ObjectMass1 = mass;
+                newForce.ObjectName1 = name1;
+
+                Console.Write("Please type in the name of the second object: ");
+                string name2 = Console.ReadLine();
+
+                Console.Write("Please type in the mass of the second object in (kg): ");
+                double mass2 = double.Parse(Console.ReadLine());
+                newForce.ObjectMass2 = mass2;
+                newForce.ObjectName2 = name2;
+
+                Console.Write("Please type in the distance apart between the objects in (meters): ");
+                double distance = double.Parse(Console.ReadLine());
+                newForce.Radius = distance;
+
+                Console.WriteLine(newForce);
+
+                //Ask to try again
+                Console.WriteLine("Would you like to try again");
+                doAgain = Console.ReadLine();
+
+            } while (doAgain == "y");
+            Console.WriteLine("Thank You. Have a Nice Day!");
+
+        }
+    }
+}
